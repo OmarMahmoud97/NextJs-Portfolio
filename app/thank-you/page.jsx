4035ceaf664d3b5e5ce2b6c6fefc353a25bb88f5
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2023-02-02 09:52:38 +0000

    add further styling to thankyou page

--- app/thank-you/page.jsx
+++ app/thank-you/page.jsx
@@ -3,9 +3,11 @@ import LiquidButton from "../../components/liquidButton/liquidButton";
 function Thankyou() {
   return (
     <div className="thanks flex-col h-screen justify-center items-center flex text-white">
-      <p className="lg:text-7xl text-5xl mb-6">Thank You</p>
-      <p className="lg:text-3xl text-2xl mb-10">Email Succesfully Sent</p>
-      <p className="lg:text-2xl text-1xl mb-10">
+      <p className="textshadow lg:text-7xl text-5xl mb-6">Thank You</p>
+      <p className="textshadow lg:text-3xl text-2xl mb-10">
+        Email Succesfully Sent
+      </p>
+      <p className="textshadow lg:text-2xl text-1xl mb-10">
         I will get back to you as soon as possibe
       </p>
       <LiquidButton />
